b1731b72c2f2bf29454b3a090560e55e38cf6e51
The Architect <thearchitectlabs+github@gmail.com>
2022-12-21 07:26:20 -0500

    Initial project commit.

---
+++ MasterMind/MasterMind/MasterMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterMindApp.swift
+//  MasterMind
+//
+//  Created by Michael & Diana Pascucci on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct MasterMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
