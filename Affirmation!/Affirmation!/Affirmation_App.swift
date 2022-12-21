eedd8839e69da5c1c7fd4a92333e373aa18e9051
The Architect <thearchitectlabs+github@gmail.com>
2022-12-21 07:30:46 -0500

    Initial project commit.

---
+++ Affirmation!/Affirmation!/Affirmation_App.swift
@@ -0,0 +1,17 @@
+//
+//  Affirmation_App.swift
+//  Affirmation!
+//
+//  Created by Michael & Diana Pascucci on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Affirmation_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
